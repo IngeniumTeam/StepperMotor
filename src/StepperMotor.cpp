61f15e2b1b2e7f6835b48e8e2b875e83f25aa3f4
Simon Pucheu <104694344+SimonPucheu@users.noreply.github.com>
2024-01-19 20:00:47 +0100

    Update

--- src/StepperMotor.cpp
+++ src/StepperMotor.cpp
@@ -18,12 +18,14 @@ StepperMotor::StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwit
 	// StepperMotor(step, dir, maxSpeed, accelTime);
 }
 
-// StepperMotor::StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwitchReversed = false, bool limitSwitchPullUp = true, int limitSwitchPin2, bool limitSwitchReversed2 = false, bool limitSwitchPullUp2 = true, int maxSpeed = 200, int accelTime = 2)
-// {
-// 	limitSwitch = Button(limitSwitchPin, limitSwitchReversed, limitSwitchPullUp);
-// 	limitSwitch2 = Button(limitSwitchPin2, limitSwitchReversed2, limitSwitchPullUp2);
-// 	StepperMotor(dir, step, maxSpeed, accelTime);
-// }
+StepperMotor::StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwitchReversed = false, bool limitSwitchPullUp = true, int limitSwitchPin2, bool limitSwitchReversed2 = false, bool limitSwitchPullUp2 = true, int maxSpeed = 200, int accelTime = 2)
+{
+	stepper = AccelStepper(AccelStepper::DRIVER, step, dir);
+	stepper.setMaxSpeed(maxSpeed);
+	stepper.setAcceleration(maxSpeed / accelTime);
+	limitSwitch = Button(limitSwitchPin, limitSwitchReversed, limitSwitchPullUp);
+	limitSwitch2 = Button(limitSwitchPin2, limitSwitchReversed2, limitSwitchPullUp2);
+}
 
 /**
  * Setup the StepperMotor
@@ -40,23 +42,23 @@ void StepperMotor::setup()
 		stepper.setSpeed(75);
 		stepper.runSpeed();
 	}
-	delay(10);
+	delay(15);
 	stepper.stop();
-	stepper.setCurrentPosition(stepper.currentPosition());
+	stepper.setCurrentPosition(0);
 }
 
 void StepperMotor::moveTo(int pos)
 {
 	stepper.moveTo(pos);
+	while (stepper.distanceToGo() != 0)
+	{
+		loop();
+	}
 }
 
 void StepperMotor::loop()
 {
-	// Serial.print(limitSwitch.getValue());
-	// Serial.print(" ");
-	// Serial.println(limitSwitch2.getValue());
-	if (limitSwitch.getValue() == LOW) {
-	// if (limitSwitch.getValue() + limitSwitch2.getValue() == LOW) {
+	if (limitSwitch.getValue() + limitSwitch2.getValue() == LOW) {
 		stepper.run();
 	}
 	else {
--- src/StepperMotor.h
+++ src/StepperMotor.h
@@ -8,7 +8,7 @@ class StepperMotor
     public:
         StepperMotor(int step, int dir, int maxSpeed = 200, int accelTime = 2);
         StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwitchReversed = false, bool limitSwitchPullUp = true, int maxSpeed = 200, int accelTime = 2);
-        // StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwitchReversed = false, bool limitSwitchPullUp = true, int limitSwitchPin2, bool limitSwitchReversed2 = false, bool limitSwitchPullUp2 = true, int maxSpeed = 200, int accelTime = 2);
+        StepperMotor(int step, int dir, int limitSwitchPin, bool limitSwitchReversed = false, bool limitSwitchPullUp = true, int limitSwitchPin2 = 0, bool limitSwitchReversed2 = false, bool limitSwitchPullUp2 = true, int maxSpeed = 200, int accelTime = 2);
         void setup();
         void moveTo(int pos);
         void loop();
